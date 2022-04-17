a815e7658bc5f113ebcb42d1650c6856ac3e2950
Mustafa Karadeniz <65789027+karadnz@users.noreply.github.com>
2022-04-18 01:22:39 +0300

    Add project files.

---
+++ stokFormLayout/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿
+namespace stokFormLayout
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnlNav = new System.Windows.Forms.Panel();
+            this.btnAyar = new System.Windows.Forms.Button();
+            this.btnGider = new System.Windows.Forms.Button();
+            this.btnTSiparis = new System.Windows.Forms.Button();
+            this.btnMSiparis = new System.Windows.Forms.Button();
+            this.btnTedarikci = new System.Windows.Forms.Button();
+            this.btnMusteri = new System.Windows.Forms.Button();
+            this.btnStok = new System.Windows.Forms.Button();
+            this.btnAnasayfa = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.panel1.Controls.Add(this.pnlNav);
+            this.panel1.Controls.Add(this.btnAyar);
+            this.panel1.Controls.Add(this.btnGider);
+            this.panel1.Controls.Add(this.btnTSiparis);
+            this.panel1.Controls.Add(this.btnMSiparis);
+            this.panel1.Controls.Add(this.btnTedarikci);
+            this.panel1.Controls.Add(this.btnMusteri);
+            this.panel1.Controls.Add(this.btnStok);
+            this.panel1.Controls.Add(this.btnAnasayfa);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(186, 577);
+            this.panel1.TabIndex = 0;
+            // 
+            // pnlNav
+            // 
+            this.pnlNav.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.pnlNav.Location = new System.Drawing.Point(0, 192);
+            this.pnlNav.Name = "pnlNav";
+            this.pnlNav.Size = new System.Drawing.Size(3, 100);
+            this.pnlNav.TabIndex = 2;
+            // 
+            // btnAyar
+            // 
+            this.btnAyar.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnAyar.FlatAppearance.BorderSize = 0;
+            this.btnAyar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAyar.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnAyar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnAyar.Image = ((System.Drawing.Image)(resources.GetObject("btnAyar.Image")));
+            this.btnAyar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnAyar.Location = new System.Drawing.Point(0, 535);
+            this.btnAyar.Name = "btnAyar";
+            this.btnAyar.Size = new System.Drawing.Size(186, 42);
+            this.btnAyar.TabIndex = 9;
+            this.btnAyar.Text = "Ayarlar";
+            this.btnAyar.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnAyar.UseVisualStyleBackColor = true;
+            this.btnAyar.Click += new System.EventHandler(this.btnAyar_Click);
+            this.btnAyar.Leave += new System.EventHandler(this.btnAyar_Leave);
+            // 
+            // btnGider
+            // 
+            this.btnGider.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnGider.FlatAppearance.BorderSize = 0;
+            this.btnGider.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGider.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnGider.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnGider.Image = ((System.Drawing.Image)(resources.GetObject("btnGider.Image")));
+            this.btnGider.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnGider.Location = new System.Drawing.Point(0, 382);
+            this.btnGider.Name = "btnGider";
+            this.btnGider.Size = new System.Drawing.Size(186, 42);
+            this.btnGider.TabIndex = 8;
+            this.btnGider.Text = "Giderler";
+            this.btnGider.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnGider.UseVisualStyleBackColor = true;
+            this.btnGider.Click += new System.EventHandler(this.btnGider_Click);
+            this.btnGider.Leave += new System.EventHandler(this.btnGider_Leave);
+            // 
+            // btnTSiparis
+            // 
+            this.btnTSiparis.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnTSiparis.FlatAppearance.BorderSize = 0;
+            this.btnTSiparis.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTSiparis.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnTSiparis.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnTSiparis.Image = ((System.Drawing.Image)(resources.GetObject("btnTSiparis.Image")));
+            this.btnTSiparis.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnTSiparis.Location = new System.Drawing.Point(0, 340);
+            this.btnTSiparis.Name = "btnTSiparis";
+            this.btnTSiparis.Size = new System.Drawing.Size(186, 42);
+            this.btnTSiparis.TabIndex = 7;
+            this.btnTSiparis.Text = "Tedarikçi Sipariş";
+            this.btnTSiparis.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnTSiparis.UseVisualStyleBackColor = true;
+            this.btnTSiparis.Click += new System.EventHandler(this.btnTSiparis_Click);
+            this.btnTSiparis.Leave += new System.EventHandler(this.btnTSiparis_Leave);
+            // 
+            // btnMSiparis
+            // 
+            this.btnMSiparis.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnMSiparis.FlatAppearance.BorderSize = 0;
+            this.btnMSiparis.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMSiparis.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnMSiparis.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnMSiparis.Image = ((System.Drawing.Image)(resources.GetObject("btnMSiparis.Image")));
+            this.btnMSiparis.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnMSiparis.Location = new System.Drawing.Point(0, 298);
+            this.btnMSiparis.Name = "btnMSiparis";
+            this.btnMSiparis.Size = new System.Drawing.Size(186, 42);
+            this.btnMSiparis.TabIndex = 6;
+            this.btnMSiparis.Text = "Müşteri Sipariş";
+            this.btnMSiparis.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnMSiparis.UseVisualStyleBackColor = true;
+            this.btnMSiparis.Click += new System.EventHandler(this.btnMSiparis_Click);
+            this.btnMSiparis.Leave += new System.EventHandler(this.btnMSiparis_Leave);
+            // 
+            // btnTedarikci
+            // 
+            this.btnTedarikci.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnTedarikci.FlatAppearance.BorderSize = 0;
+            this.btnTedarikci.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTedarikci.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnTedarikci.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnTedarikci.Image = ((System.Drawing.Image)(resources.GetObject("btnTedarikci.Image")));
+            this.btnTedarikci.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnTedarikci.Location = new System.Drawing.Point(0, 256);
+            this.btnTedarikci.Name = "btnTedarikci";
+            this.btnTedarikci.Size = new System.Drawing.Size(186, 42);
+            this.btnTedarikci.TabIndex = 5;
+            this.btnTedarikci.Text = "Tedarikçi";
+            this.btnTedarikci.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnTedarikci.UseVisualStyleBackColor = true;
+            this.btnTedarikci.Click += new System.EventHandler(this.btnTedarikci_Click);
+            this.btnTedarikci.Leave += new System.EventHandler(this.btnTedarikci_Leave);
+            // 
+            // btnMusteri
+            // 
+            this.btnMusteri.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnMusteri.FlatAppearance.BorderSize = 0;
+            this.btnMusteri.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMusteri.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnMusteri.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnMusteri.Image = ((System.Drawing.Image)(resources.GetObject("btnMusteri.Image")));
+            this.btnMusteri.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnMusteri.Location = new System.Drawing.Point(0, 214);
+            this.btnMusteri.Name = "btnMusteri";
+            this.btnMusteri.Size = new System.Drawing.Size(186, 42);
+            this.btnMusteri.TabIndex = 4;
+            this.btnMusteri.Text = "Müşteri";
+            this.btnMusteri.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnMusteri.UseVisualStyleBackColor = true;
+            this.btnMusteri.Click += new System.EventHandler(this.btnMusteri_Click);
+            this.btnMusteri.Leave += new System.EventHandler(this.btnMusteri_Leave);
+            // 
+            // btnStok
+            // 
+            this.btnStok.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnStok.FlatAppearance.BorderSize = 0;
+            this.btnStok.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStok.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnStok.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnStok.Image = ((System.Drawing.Image)(resources.GetObject("btnStok.Image")));
+            this.btnStok.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnStok.Location = new System.Drawing.Point(0, 172);
+            this.btnStok.Name = "btnStok";
+            this.btnStok.Size = new System.Drawing.Size(186, 42);
+            this.btnStok.TabIndex = 3;
+            this.btnStok.Text = "Stok";
+            this.btnStok.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnStok.UseVisualStyleBackColor = true;
+            this.btnStok.Click += new System.EventHandler(this.btnStok_Click);
+            this.btnStok.Leave += new System.EventHandler(this.btnStok_Leave);
+            // 
+            // btnAnasayfa
+            // 
+            this.btnAnasayfa.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnAnasayfa.FlatAppearance.BorderSize = 0;
+            this.btnAnasayfa.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAnasayfa.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnAnasayfa.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnAnasayfa.Image = ((System.Drawing.Image)(resources.GetObject("btnAnasayfa.Image")));
+            this.btnAnasayfa.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnAnasayfa.Location = new System.Drawing.Point(0, 130);
+            this.btnAnasayfa.Name = "btnAnasayfa";
+            this.btnAnasayfa.Size = new System.Drawing.Size(186, 42);
+            this.btnAnasayfa.TabIndex = 2;
+            this.btnAnasayfa.Text = "Ana sayfa";
+            this.btnAnasayfa.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnAnasayfa.UseVisualStyleBackColor = true;
+            this.btnAnasayfa.Click += new System.EventHandler(this.btnAnasayfa_Click);
+            this.btnAnasayfa.Leave += new System.EventHandler(this.btnAnasayfa_Leave);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(186, 130);
+            this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(156)))), ((int)(((byte)(149)))));
+            this.label1.Location = new System.Drawing.Point(58, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kullanici";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(60, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(63, 63);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(951, 577);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnGider;
+        private System.Windows.Forms.Button btnTSiparis;
+        private System.Windows.Forms.Button btnMSiparis;
+        private System.Windows.Forms.Button btnTedarikci;
+        private System.Windows.Forms.Button btnMusteri;
+        private System.Windows.Forms.Button btnStok;
+        private System.Windows.Forms.Button btnAnasayfa;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel pnlNav;
+        private System.Windows.Forms.Button btnAyar;
+    }
+}
+
